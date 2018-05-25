b7a86dda93fca768e75a5ccf164fe8cc6a1eb656
Malachi Irwin <malachi@theirwins.ws>
2018-05-25 09:49:09 -0500

    This commit will Add code for ones column to work

--- spec/numbers_to_roman_numerals_spec.rb
+++ spec/numbers_to_roman_numerals_spec.rb
@@ -4,6 +4,6 @@ require "numbers_to_roman_numerals"
 describe('Numbers_to_roman_numerals') do
   it('changes a given number to roman numerals') do
     number = Number.new()
-    expect(number.to_roman_numerals(13)).to eq('XIII')
+    expect(number.to_roman_numerals(4)).to eq('IV')
   end
 end
